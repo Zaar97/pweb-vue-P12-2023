c919f13a07b8317c7becadce0b273483e334c442
Zaar97 <128958228+Zaar97@users.noreply.github.com>
2023-10-02 21:16:41 +0700

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/pweb-vue-P12-2023',
+  base: '/pweb-vue-P12-2023/',
   plugins: [
     vue(),
   ],
